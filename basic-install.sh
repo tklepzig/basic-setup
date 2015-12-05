a08ccb926715b3293823dfdac39e8b73a3c34216
Thomas <Thomas@DevBook>
2015-12-05 18:36:05 +0100

    added explicit sudo to all necessary commands due to errors of non-sudo commands, configured git diff for using always word diff

--- basic-install.sh
+++ basic-install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-apt-get -y update
-apt-get -y install joe git npm nodejs-legacy
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+sudo apt-get -y update
+sudo apt-get -y install joe git npm nodejs-legacy
 
 if [ -f atom-packages ]; then
-    cp atom-packages /tmp/atom-packages
+    cp atom-packages /tmp
 fi
 
 cd /tmp
 
 if [ ! -f google-chrome-stable_current_amd64.deb ]; then
-    wget  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 fi
 
 if [ ! -f deb ]; then
     wget https://atom.io/download/deb
 fi
 
-dpkg -i google-chrome-stable_current_amd64.deb
-dpkg -i deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i deb
 
-apt-get -fy install
+sudo apt-get -fy install
 
-npm install -g bower gulp-cli ungit live-server nodemon
+sudo npm install -g bower gulp-cli ungit live-server nodemon
 
 if [ -f atom-packages ]; then
     echo "Installing atom packages"
