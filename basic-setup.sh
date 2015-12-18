66c86b54084ca0b9b8e4a1c03e939e8acb21c3f8
Thomas <Thomas@DevBook>
2015-12-18 17:33:13 +0100

    ask all questions at beginning

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+install_development=false;
+install_multimedia=false;
+
+echo "Do you wish to install development stuff?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) install_development=true; break;;
+        No ) install_development=false; break;;
+    esac
+done
+
+echo "Do you wish to install multimedia stuff?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) install_multimedia=true; break;;
+        No ) install_multimedia=false; break;;
+    esac
+done
+
+
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 sudo apt-get -y update
 sudo apt-get -y install joe unity-tweak-tool
@@ -9,44 +29,28 @@ if [ -f chrome.sh ]; then
     ./chrome.sh
 fi
 
+if $install_development; then
+    sudo apt-get -y install vim git meld npm nodejs-legacy
+    sudo npm install -g bower gulp-cli ungit live-server nodemon
 
+    if [ -f git-config.sh ]; then
+        echo "Configure git environment"
+        ./git-config.sh
+    fi
 
-echo "Do you wish to install development stuff?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
+    if [ -f atom.sh ]; then
+        echo "Installing Atom"
+        ./atom.sh
+    fi
 
-sudo apt-get -y install vim git meld npm nodejs-legacy
-sudo npm install -g bower gulp-cli ungit live-server nodemon
-
-if [ -f git-config.sh ]; then
-    echo "Configure git environment"
-    ./git-config.sh
+    if [ -f atom-packages ]; then
+        echo "Installing atom packages"
+        apm install --packages-file atom-packages
+    fi
 fi
 
-if [ -f atom.sh ]; then
-    echo "Installing Atom"
-    ./atom.sh
+if $install_multimedia; then
+    sudo apt-get -y install winff easytag audacity gimp vlc
 fi
-
-if [ -f atom-packages ]; then
-    echo "Installing atom packages"
-    apm install --packages-file atom-packages
-fi
-
-
-
-echo "Do you wish to install multimedia stuff?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
-done
-
-sudo apt-get -y install winff easytag audacity gimp vlc
 
 exit
