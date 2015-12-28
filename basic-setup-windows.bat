edd1c8dffb27c3bbbe6a5ad00ce3423e682ddf74
Thomas <Thomas@DevBook>
2015-12-28 01:17:51 +0100

    some minor changes in windows installing script

--- basic-setup-windows.bat
+++ basic-setup-windows.bat
@@ -1,5 +1,10 @@
-echo "Install Chocolatey"
+@echo off
+
+echo Installing Chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install -y atom googlechrome audacity git.install meld nodejs.install winff gimp vlc mp3tag
+echo Installing choco packages
+choco install -y atom googlechrome audacity git meld nodejs winff gimp vlc mp3tag
+
+echo Installing npm packages
 npm install -g bower gulp-cli ungit live-server nodemon
