319bd5fff8804d2188f97dbdacbb7901c168216d
Thomas <Thomas@DevBook>
2016-03-20 18:53:26 +0100

    added bashrc config to script, removed .bashrc file

--- basic-setup.sh
+++ basic-setup.sh
@@ -24,6 +24,24 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)
 sudo apt-get -y update
 sudo apt-get -y install vim joe unity-tweak-tool xdotool
 
+
+echo "Configuring .bashrc"
+if ! grep -q "~/.custom-config" ~/.bashrc
+then
+    echo "if [ -f ~/.custom-config ]; then . ~/.custom-config; fi" >> ~/.bashrc;
+fi
+rm -f ~/.custom-config
+echo "alias mkcd='function __mkcd() { mkdir \"\$1\"; cd \"\$1\"; unset -f __mkcd; }; __mkcd'" >> ~/.custom-config
+echo "alias git='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git'" >> ~/.custom-config
+echo "alias g='git'" >> ~/.custom-config
+echo "alias gk='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 gitk --all &'" >> ~/.custom-config
+echo "alias gg='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git gui &'" >> ~/.custom-config
+echo "alias a='atom .'" >> ~/.custom-config
+echo "#show current branch and possible staged or unstaged changes in bash prompt" >> ~/.custom-config
+echo "export GIT_PS1_SHOWDIRTYSTATE=1" >> ~/.custom-config
+echo "export PS1='\u@\h:\w\[\033[01;32m\]\$(__git_ps1)\[\033[00m\]\\$ '" >> ~/.custom-config
+
+
 if [ -f chrome.sh ]; then
     echo "Installing Google Chrome"
     ./chrome.sh
@@ -31,16 +49,16 @@ fi
 
 if $install_development; then
     sudo apt-get -y install git git-gui meld kdiff3 npm nodejs-legacy
-    
+
     #install latest version of git
     sudo add-apt-repository ppa:git-core/ppa
     sudo apt-get update
     sudo apt-get install git
-    
+
     #install latest version of node
     curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
     sudo apt-get install -y nodejs
-    
+
     sudo npm install -g bower gulp-cli grunt-cli ungit live-server nodemon node-inspector livedown
 
     if [ -f git-config.sh ]; then
