e6b723a0fd76848ff0605abcc97dcc8dd7529fb4
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-04-08 07:23:36 +0200

    Update basic-setup.sh

--- basic-setup.sh
+++ basic-setup.sh
@@ -60,7 +60,7 @@ if $install_development; then
     curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
     sudo apt-get install -y nodejs
 
-    sudo npm install -g bower gulp-cli grunt-cli ungit live-server nodemon node-inspector livedown
+    sudo npm install -g bower gulp-cli grunt-cli ungit live-server nodemon node-inspector livedown mocha
 
     if [ -f git-config.sh ]; then
         echo "Configure git environment"
