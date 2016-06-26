12935d71e162dad9280f7f33ba9a8efcb97eddf2
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-06-26 19:47:59 +0200

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -2,3 +2,5 @@
 
 code --install-extension christian-kohler.path-intellisense
 code --install-extension ms-vscode.csharp
+code --install-extension anseki.vscode-color
+code --install-extension robertohuertasm.vscode-icons
