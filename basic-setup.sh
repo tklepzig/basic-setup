5d636c5f8b1d1f0a2ac5f456f2e0a27c0258e884
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-08-18 09:54:09 +0200

    Update basic-setup.sh

--- basic-setup.sh
+++ basic-setup.sh
@@ -53,6 +53,7 @@ echo "export GIT_PS1_SHOWDIRTYSTATE=1" >> ~/.custom-config
 echo "export PS1='\u@\h:\[\033[0;33m\]\w\[\033[01;32m\]\$(__git_ps1)\[\033[00m\]\n\\$ '" >> ~/.custom-config
 echo "export PROMPT_DIRTRIM=4" >> ~/.custom-config
 echo "shopt -s extglob" >> ~/.custom-config
+echo "__git_complete g _git" >> ~/.custom-config
 
 if [ -f chrome.sh ]; then
     echo -e "${accent}Installing Google Chrome${normal}"
