173b9175c442eb770850f464e662dd42449b25e8
Klepzig <Thomas.Klepzig@pcw.local>
2016-09-19 13:13:23 +0200

    updated packages for atom and code

--- vscode.sh
+++ vscode.sh
@@ -3,7 +3,6 @@
 code --install-extension christian-kohler.path-intellisense
 code --install-extension ms-vscode.csharp
 code --install-extension anseki.vscode-color
-code --install-extension robertohuertasm.vscode-icons
 code --install-extension donjayamanne.githistory
 code --install-extension xabikos.ReactSnippets
 code --install-extension maptz.camelcasenavigation
