bc66a2649124aa1039c01ad9607371043e44dfe4
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-12-06 09:08:42 +0100

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 code --install-extension christian-kohler.path-intellisense
-code --install-extension ms-vscode.csharp
 code --install-extension anseki.vscode-color
 code --install-extension donjayamanne.githistory
-code --install-extension xabikos.ReactSnippets
 code --install-extension maptz.camelcasenavigation
+code --install-extension mrmlnc.vscode-scss
+code --install-extension michelemelluso.code-beautifier
