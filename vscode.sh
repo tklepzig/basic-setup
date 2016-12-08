222b68cb3dd39eee99a7de350c1c15ad7fa0bc4c
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-12-08 20:47:17 +0100

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -6,3 +6,7 @@ code --install-extension donjayamanne.githistory
 code --install-extension maptz.camelcasenavigation
 code --install-extension mrmlnc.vscode-scss
 code --install-extension michelemelluso.code-beautifier
+code --install-extension ms-vscode.csharp
+code --install-extension cake-build.cake-vscode
+code --install-extension SirTobi.code-clip-ring
+code --install-extension MattiasPernhult.vscode-todo
