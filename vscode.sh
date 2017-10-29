19fd4712856cf9e8e505b40627e6658fa184bd9e
Thomas <Thomas@DevDisc>
2017-10-29 13:01:47 +0100

    updated vsc extensions

--- vscode.sh
+++ vscode.sh
@@ -4,13 +4,9 @@ code-insiders --install-extension WallabyJs.quokka-vscode
 code-insiders --install-extensionformulahendry.auto-rename-tag
 code-insiders --install-extension akamud.vscode-javascript-snippet-pack
 code-insiders --install-extension christian-kohler.path-intellisense
-code-insiders --install-extension cmstead.jsrefactor
-code-insiders --install-extension dbaeumer.vscode-eslint
 #code-insiders --install-extension donjayamanne.githistory
 code-insiders --install-extension huizhou.githd
-code-insiders --install-extension dzannotti.vscode-babel-coloring
 code-insiders --install-extension eamodio.gitlens
-code-insiders --install-extension jeremyrajan.react-component
 code-insiders --install-extension kisstkondoros.typelens
 code-insiders --install-extension michelemelluso.code-beautifier
 code-insiders --install-extension minhthai.vscode-todo-parser
@@ -23,20 +19,26 @@ code-insiders --install-extension maptz.camelcasenavigation
 code-insiders --install-extension nwhatt.chai-snippets
 code-insiders --install-extension spoonscen.es6-mocha-snippets
 
+# ES 2015, Babel
+code-insiders --install-extension dzannotti.vscode-babel-coloring
+code-insiders --install-extension dbaeumer.vscode-eslint
+code-insiders --install-extension cmstead.jsrefactor
+code-insiders --install-extension jeremyrajan.react-component
+
+# Typescript
+code-insiders --install-extension rbbit.typescript-hero # Ein Typescript Import Assistant, der alle Imports einer Datei auf einmal auflösen kann
+code-insiders --install-extension pmneo.tsimporter # Ebenfalls ein Typescript Import Assistant, aber der unterstützt die tsconfig.json
+code-insiders --install-extension eg2.tslint
+code-insiders --install-extension christianoetterli.refactorix
+code-insiders --install-extension infeng.vscode-react-typescript
+
+# Angular
+#code-insiders --install-extension johnpapa.Angular2
+#code-insiders --install-extension Angular.ng-template
 
 # C#
 #code-insiders --install-extension ms-vscode.csharp
 #code-insiders --install-extension cake-build.cake-vscode
-
-# Angular
-#code-insiders --install-extension johnpapa.Angular2
-# Ein Typescript Import Assistant, der alle Imports einer Datei auf einmal auflösen kann
-#code-insiders --install-extension rbbit.typescript-hero
-# Ebenfalls ein Typescript Import Assistant, aber der unterstützt die tsconfig.json
-#code-insiders --install-extension pmneo.tsimporter
-# Angular Support in HTML files
-#code-insiders --install-extension Angular.ng-template
-#code-insiders --install-extension eg2.tslint
 
 exit
 
@@ -64,6 +66,16 @@ exit
     {
         "key": "ctrl+backspace",
         "command": "extension.deleteCamelLeftCommand",
+        "when": "editorTextFocus"
+    },
+    {
+        "key": "ctrl+shift+i",
+        "command": "-typescriptHero.resolve.addImport",
+        "when": "editorTextFocus"
+    },
+    {
+        "key": "ctrl+shift+alt+i",
+        "command": "-typescriptHero.resolve.addMissingImports",
         "when": "editorTextFocus"
     }
 ]
