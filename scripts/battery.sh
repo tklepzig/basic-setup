197fbc40312b518cd1c9dc54ae244bb7889d8a17
Thomas <thomas@DevPad>
2019-10-05 23:46:34 +0200

    Move docs and tools to dotfiles

--- scripts/battery.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watch -n 10 "cat /sys/class/power_supply/BAT1/uevent|grep ENERGY"
--- tools/seafile.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository ppa:seafile/seafile-client
-sudo apt-get update
-sudo apt-get install seafile-gui
